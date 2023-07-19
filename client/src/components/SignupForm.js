e831e750c6a6c6b52b21ca35685f943b1a224f48
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-07-18 21:17:39 -0500

    modifying files in client and server folder

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -6,12 +6,12 @@ import { ADD_USER } from '../utils/mutations';
 
 
 const SignupForm = () => {
-  // set initial form state
+  
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
 
-  // set state for form validation
+  
   const [validated] = useState(false);
-  // set state for alert
+  
   const [showAlert, setShowAlert] = useState(false);
 
   const [addUser] = useMutation(ADD_USER);
@@ -24,7 +24,7 @@ const SignupForm = () => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-    // check if form has everything (as per react-bootstrap docs)
+    
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.preventDefault();
@@ -37,7 +37,7 @@ const SignupForm = () => {
       });
 
       const { user } = data;
-      // console.log(user);
+      
       Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
@@ -53,9 +53,9 @@ const SignupForm = () => {
 
   return (
     <>
-      {/* This is needed for the validation functionality above */}
+      {}
       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-        {/* show alert if server response is bad */}
+        {}
         <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
           Something went wrong with your signup!
         </Alert>
--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -13,9 +13,9 @@ const SavedBooks = () => {
   const [ deleteBook ] = useMutation(REMOVE_BOOK);
 
   const userData = data?.me || [];
-  // console.log(userData);
+  
 
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
+ 
   const handleDeleteBook = async (bookId) => {
 
     const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -25,20 +25,20 @@ const SavedBooks = () => {
     }
 
     try {
-      // const response = await deleteBook(bookId, token);
+      
       const { data } = await deleteBook({
         variables: { bookId }
       });
 
 
-      // upon success, remove book's id from localStorage
+     
       removeBookId(bookId);
     } catch (err) {
       console.error(err);
     }
   };
 
-  // if data isn't here yet, say so
+  
   if (loading) {
     return <h2>LOADING...</h2>;
   }
